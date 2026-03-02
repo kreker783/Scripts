e4abff1e3d85bcbcdd4f77d612084b95632d0714
Main OS <kreker783@gmail.com>
2026-03-02 21:54:54 +0100

    Continue

--- Linux/getInfo/getInfo.py
+++ Linux/getInfo/getInfo.py
@@ -1,17 +1,34 @@
-import os
-import sys
+import platform
 import socket
+
 
 def get_hostname():
     return socket.gethostname()
 
-def get_IP():
-    return socket.gethostbyname(get_hostname)
 
+def get_ip():
+    return socket.gethostbyname(get_hostname())
+
+def get_os():
+    return platform.system()
+
+def get_cpu():
+    return platform.machine()
+
+def get_python_version():
+    return platform.python_version()
+
+def get_system_info():
+    return platform.uname()
 
 def main():
-    print(f'Hostname: {get_hostname}')
-    print(f'IP: {get_IP}')
+    print(f'Hostname: {get_hostname()}')
+    print(f'IP: {get_ip()}')
+    print(f'OS: {get_os()}')
+    print(f'CPU: {get_cpu()}')
+    print(f'Python version: {get_python_version()}')
+    print(f'System release: {get_system_info().release}')
+
 
 if __name__ == "__main__":
     main()
