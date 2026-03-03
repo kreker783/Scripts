dc125d52aebb892f3987c2a7ac69136f3f48702b
Main OS <kreker783@gmail.com>
2026-03-03 21:52:01 +0100

    Create gitignores Expand export file, need to correct names

--- Python/virtualBox/export_vbox_vms_all.py
+++ Python/virtualBox/export_vbox_vms_all.py
@@ -1,13 +1,37 @@
 import subprocess
 import os
-import argparse
 
-output_directory = '/home/kreker/personal/vm_backup'
+currentUser = os.environ['USER']
+
+output_directory = f'/home/{currentUser}/Documents/vmBackup\n'
+
+def check_output_directory():
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    print(f"Output directory: {output_directory}")
+
+def get_vms():
+    vms = subprocess.run(['VBoxManage', 'list', 'vms'], capture_output=True, text=True)
+    output = vms.stdout
+    vms_list = [line.split(' ')[0].strip('"') for line in output.splitlines()]
+    return vms_list
+
+def check_vm_state(vm_name):
+    vm_state = subprocess.run(['VBoxManage', 'showvminfo', vm_name], capture_output=True, text=True)
+    print(vm_state.stdout)
+    if "running" in vm_state.stdout:
+        print(f"VM {vm_name} is running. Please shut it down before exporting.")
+        return False
+    return True
+
+def main():
+    check_output_directory()
+
+    list_of_vms = get_vms()
+    for vm_name in list_of_vms:
+        print(f"VM to export: {vm_name}")
+        check_vm_state(vm_name)
 
 
-get_vms = subprocess.run(['VBoxManage', 'list', 'vms'], capture_output=True, text=True)
-output = get_vms.stdout
-vms_list = [line.split(' ')[0].strip('"') for line in output.splitlines()]
-
-for vm_name in vms_list:
-    print(f"VM to export: {vm_name}")
+if __name__ == "__main__":
+    main()
