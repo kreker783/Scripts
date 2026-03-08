cdb479e5a902fc096409df8335a52c6a8b0275bb
Main OS <kreker783@gmail.com>
2026-03-08 21:37:24 +0100

    Finished import script

--- Python/virtualBox/import_vbox_vms.py
+++ Python/virtualBox/import_vbox_vms.py
@@ -1,9 +1,16 @@
 import os
 import subprocess
-
+import sys
 
 currentUser = os.environ['USER']
-import_directory = f'/home/{currentUser}/Documents/vmBackup\n'
+import_directory = f'/home/{currentUser}/Documents/vmBackup'
+
+def check_arguments():
+    if len(sys.argv) == 1:
+        print("No VM name provided. All VMs will be imported.")
+        return None
+    else:
+        return sys.argv[1:]
 
 def check_VBoxManage():
     if subprocess.run(['VBoxManage', '-v'], capture_output=True, text=True).returncode != 0:
@@ -17,10 +24,30 @@ def check_import_directory():
         exit()
     print(f"Import from: {import_directory}")
 
+def get_vms():
+    files = os.listdir(import_directory)
+    return files
+
+def import_vm(vm_name, import_directory=import_directory):
+    subprocess.run(['VBoxManage', 'import', f'{import_directory}/{vm_name}'])
+
 
 def main():
     check_VBoxManage()
     check_import_directory()
+
+    provided_vms_to_import = check_arguments()
+
+    if provided_vms_to_import is None:
+        vms_to_import = get_vms()
+    else:
+        vms_to_import = provided_vms_to_import
+
+    for vm_name in vms_to_import:
+        import_vm(vm_name)
+
+    print("All VMs imported.")
+
 
 if __name__ == "__main__":
     main()
